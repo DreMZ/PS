8e8825d12cb4049b382b79a6d8f420af7b494ab1
Nollan101 <kgb@kgbudge.com>
2013-04-02 21:00:25 -0600

    Update scripts.js

--- mods/nuv2/scripts.js
+++ mods/nuv2/scripts.js
@@ -3,42 +3,42 @@ exports.BattleScripts = {
                 for (var i in this.data.FormatsData)
                         this.data.FormatsData[i].dreamWorldRelease = true;
                         
-this.data.Learnsets.serperior.learnset.zenheadbutt = ['5L0'];}
-this.data.Learnsets.serperior.learnset.psychoboost = ['5L0'];}
-this.data.Learnsets.serperior.learnset.naturepower = ['5L0'];}
-this.data.Learnsets.serperior.learnset.earthpower = ['5L0'];}
-this.data.Learnsets.serperior.learnset.lunardance = ['5L0'];}
-this.data.Learnsets.serperior.learnset.psystrike = ['5L0'];}
-this.data.Learnsets.serperior.learnset.aurasphere = ['5L0'];}
-this.data.Learnsets.articuno.learnset.airslash = ['5L0'];}
-this.data.Learnsets.articuno.learnset.earthpower = ['5L0'];}
-this.data.Learnsets.vulpix.learnset.psychic = ['5L0'];}
-this.data.Learnsets.vulpix.learnset.psyshock = ['5L0'];}
-this.data.Learnsets.vulpix.learnset.morningsun = ['5L0'];}
-this.data.Learnsets.vulpix.learnset.wish = ['5L0'];}
-this.data.Learnsets.carracosta.learnset.xscissor = ['5L0'];}
-this.data.Learnsets.carracosta.learnset.headsmash = ['5L0'];}
-this.data.Learnsets.torterra.learnset.shellsmash = ['5L0'];}
-this.data.Learnsets.torterra.learnset.rockblast = ['5L0'];}
-this.data.Learnsets.girafarig.learnset.bulkup = ['5L0'];}
-this.data.Learnsets.girafarig.learnset.focusblast = ['5L0'];}
-this.data.Learnsets.girafarig.learnset.shadowclaw = ['5L0'];}
-this.data.Learnsets.masquerain.learnset.earthpower = ['5L0'];}
-delete this.data.Learnsets.simipour.learnset.nastyplot;}
-{this.data.Learnsets.simipour.learnset.wish = ['5L0'];}
-this.data.Learnsets.simipour.learnset.calmmind = ['5L0'];}
-this.data.Learnsets.simipour.learnset.futuresight = ['5L0'];}
-this.data.Learnsets.simipour.learnset.shadowball = ['5L0'];}
-this.data.Learnsets.simipour.learnset.psychic = ['5L0'];}
-this.data.Learnsets.simipour.learnset.psyshock = ['5L0'];}
-this.data.Learnsets.simipour.learnset.lightscreen = ['5L0'];}
-this.data.Learnsets.simipour.learnset.reflect = ['5L0'];}
-delete this.data.Learnsets.simisage.learnset.nastyplot;}
-this.data.Learnsets.simisage.learnset.leafblade = ['5L0'];}
-this.data.Learnsets.simisage.learnset.bulkup = ['5L0'];}
-this.data.Learnsets.simisage.learnset.naturepower = ['5L0'];}
-this.data.Learnsets.simisage.learnset.closecombat = ['5L0'];}
-delete this.data.Learnsets.simisear.learnset.nastyplot;}
+this.data.Learnsets.serperior.learnset.zenheadbutt = ['5L0'];
+this.data.Learnsets.serperior.learnset.psychoboost = ['5L0'];
+this.data.Learnsets.serperior.learnset.naturepower = ['5L0'];
+this.data.Learnsets.serperior.learnset.earthpower = ['5L0'];
+this.data.Learnsets.serperior.learnset.lunardance = ['5L0'];
+this.data.Learnsets.serperior.learnset.psystrike = ['5L0'];
+this.data.Learnsets.serperior.learnset.aurasphere = ['5L0'];
+this.data.Learnsets.articuno.learnset.airslash = ['5L0'];
+this.data.Learnsets.articuno.learnset.earthpower = ['5L0'];
+this.data.Learnsets.vulpix.learnset.psychic = ['5L0'];
+this.data.Learnsets.vulpix.learnset.psyshock = ['5L0'];
+this.data.Learnsets.vulpix.learnset.morningsun = ['5L0'];
+this.data.Learnsets.vulpix.learnset.wish = ['5L0'];
+this.data.Learnsets.carracosta.learnset.xscissor = ['5L0'];
+this.data.Learnsets.carracosta.learnset.headsmash = ['5L0'];
+this.data.Learnsets.torterra.learnset.shellsmash = ['5L0'];
+this.data.Learnsets.torterra.learnset.rockblast = ['5L0'];
+this.data.Learnsets.girafarig.learnset.bulkup = ['5L0'];
+this.data.Learnsets.girafarig.learnset.focusblast = ['5L0'];
+this.data.Learnsets.girafarig.learnset.shadowclaw = ['5L0'];
+this.data.Learnsets.masquerain.learnset.earthpower = ['5L0'];
+delete this.data.Learnsets.simipour.learnset.nastyplot;
+{this.data.Learnsets.simipour.learnset.wish = ['5L0'];
+this.data.Learnsets.simipour.learnset.calmmind = ['5L0'];
+this.data.Learnsets.simipour.learnset.futuresight = ['5L0'];
+this.data.Learnsets.simipour.learnset.shadowball = ['5L0'];
+this.data.Learnsets.simipour.learnset.psychic = ['5L0'];
+this.data.Learnsets.simipour.learnset.psyshock = ['5L0'];
+this.data.Learnsets.simipour.learnset.lightscreen = ['5L0'];
+this.data.Learnsets.simipour.learnset.reflect = ['5L0'];
+delete this.data.Learnsets.simisage.learnset.nastyplot;
+this.data.Learnsets.simisage.learnset.leafblade = ['5L0'];
+this.data.Learnsets.simisage.learnset.bulkup = ['5L0'];
+this.data.Learnsets.simisage.learnset.naturepower = ['5L0'];
+this.data.Learnsets.simisage.learnset.closecombat = ['5L0'];
+delete this.data.Learnsets.simisear.learnset.nastyplot;
 this.data.Learnsets.simisear.learnset.darkpulse = ['5L0'];
 this.data.Learnsets.simisear.learnset.earthpower = ['5L0'];
 this.data.Learnsets.simisear.learnset.extrasensory = ['5L0'];
