b9c5e36e46d417767425dd9650e3ad7095c4e176
Oiawesome <Oiawesome99@yahoo.com>
2013-04-02 20:54:40 -0700

    Update moves.js

--- mods/nuv2/moves.js
+++ mods/nuv2/moves.js
@@ -16,26 +16,26 @@
         }
     },
         
-        "lunardance": {
-                num: 461
-                accuracy: true,
-                basePower: 0,
-                category: "Status",
-                des: "The user faints and the Pokemon brought out to replace it has its HP and PP fully restored aling with having any major status condition cured. Fails if  the user is the last unfainted Pokemon in its party.",
-                shortDesc: "User faints. Replacement is fully healed, with PP.",
-                id: "lunardance",
-                isViable: true,
-                name: "Lunar Dance",
-                pp: 20,
-                priority: 0,
-                isSnatchable: true,
-                boosts: {
-                                spa: 1,
-                                spe: 1
-                },
-                secondary: false,
-                target: "self",
-                type: "Psychic"
+          lunardance: {
+		num: 461,
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		desc: "The user faints and the Pokemon brought out to replace it has its HP and PP fully restored along with having any major status condition cured. Fails if the user is the last unfainted Pokemon in its party.",
+		shortDesc: "User faints. Replacement is fully healed, with PP.",
+		id: "lunardance",
+		isViable: true,
+		name: "Lunar Dance",
+		pp: 20,
+		priority: 0,
+		isSnatchable: true,
+		boosts: {
+				spa: 1,
+				spe: 1
+		},
+		secondary: false,
+		target: "self",
+		type: "Psychic"
         }
     },
         
