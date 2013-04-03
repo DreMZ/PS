eb75002cfc198a80443a097ad888a3e8ff0de2b1
Nollan101 <kgb@kgbudge.com>
2013-04-02 19:55:48 -0600

    Update abilities.js
    added Zen Mode

--- mods/duskmod/abilities.js
+++ mods/duskmod/abilities.js
@@ -287,3 +287,21 @@ exports.BattleAbilities = {
 		}
 	},	
 };
+	"zenmode": {
+		desc: "When Darmanitan enters the battle, it will enter Zen Mode. This ability only works on Darmanitan, even if it is copied by Role Play, Entrainment, or swapped with Skill Swap.",
+		shortDesc: "If this Pokemon is Darmanitan, it changes to Zen Mode.",
+			onStart: function(pokemon) {
+			if (pokemon.template.speciesid==='darmanitan' && pokemon.transformInto('Darmanitan-Zen')) {
+			pokemon.transformed = false;
+			this.add('-formechange', pokemon, 'Darmanitan-Zen');
+			this.add('-message', 'Zen Mode triggered! (placeholder)');
+			} else {
+			return false;
+		}
+	},
+		id: "zenmode",
+		name: "Zen Mode",
+		rating: 3,
+		num: 161
+	}
+};
