d1175fcae2d5c824053273c0cbeeafc0f4d17d30
Nollan101 <kgb@kgbudge.com>
2013-04-02 19:10:11 -0600

    Update formats.js
    Input DM1 and DM2 formats.

--- data/formats.js
+++ data/formats.js
@@ -11,7 +11,29 @@ exports.BattleFormats = {
                 ruleset: ['RU'],
                 banlist: ['RU', 'Beheeyem', 'Braviary', 'Gardevoir', 'Musharna', 'Luxray', 'Roselia', 'Drizzle', 'Drought', 'Sand Rush + Torterra + Shell Smash']
         },
-
+	
+	DM2OU: {
+		effectType: 'Format',
+		name: "DM2",
+		mod: "DM2",
+		challengeShow: true,
+		searchShow: true,
+		isTeambuilderFormat: true,
+		ruleset: ['OU']
+		banlist: {'Kyurem', 'Kyurem-Black', 'Arceus-Bug', 'Arceus-Dark', 'Arceus-Dragon', 'Arceus-Electric', 'Arceus-Fighting', 'Arceus-Fire', 'Arceus-Flying', 'Arceus-Ghost', 'Arceus-Grass', 'Arceus-Ground', 'Arceus-Ice', 'Arceus-Normal', 'Arceus-Poison', 'Arceus-Psychic', 'Arceus-Rock', 'Arceus-Steel', 'Arceus-Water', 'Deoxys', 'Deoxys-Attack', 'Deoxys-Defense', 'Deoxys-Speed', 'Dialga', 'Giratina', 'Giratina-Origin', 'Groudon', 'Ho-oh', 'Kyogre', 'Kyurem', 'Kyurem-Black', 'Kyurem-White', 'Lugia', 'Manaphy', 'Mewtwo', 'Palkia', 'Rayquaza', 'Reshiram', 'Shaymin-Sky', 'Zekrom', 'Soul Dew'}
+	}
+	
+	DMOU: {
+		effectType: 'Format',
+		name: "DM"
+		mod: "DM"
+		challengeShow: true,
+		searchShow: true,
+		isTeambuilderFormat: true,
+		ruleset: ['OU']
+		banlist: {'Uber', 'Kyurem', 'Kyurem-B', 'Regigigas', 'Soul Dew'}
+	}
+	
 	randombattle: {
 		effectType: 'Format',
 		name: "Random Battle",
