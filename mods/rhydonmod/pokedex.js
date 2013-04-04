713ee878532b053763008cecc6ff10c864763751
PokermansCoach <EngineerBLT@hotmail.com>
2013-04-04 23:42:58 +0200

    Create pokedex.js

---
+++ mods/rhydonmod/pokedex.js
@@ -0,0 +1,4 @@
+exports.BattlePokedex = {
+rhydon:{num:112,species:"Rhydon",types:["Ground","Rock"],baseStats:{hp:210,atk:260,def:240,spa:90,spd:90,spe:80},abilities:{0:"Lightningrod",1:"Rock Head",DW:"Reckless"},heightm:1.9,weightkg:120,color:"Gray",prevo:"rhyhorn",evos:["rhyperior"],evoLevel:42,eggGroups:["Monster","Ground"]},
+
+};
