101aab7b5fe2012f4ecd19d502150eb2a9306c48
PokermansCoach <EngineerBLT@hotmail.com>
2013-04-04 04:26:30 +0200

    Create format-data.js

---
+++ mods/randomtypings/format-data.js
@@ -0,0 +1,22 @@
+exports.BattleFormatsData = {
+  deoxysspeed: {
+		inherit: true,
+		tier: 'OU'
+	},
+	manaphy: {
+		inherit: true,
+		tier: 'OU'
+	},
+	excadrill: {
+		inherit: true,
+		tier: 'OU'
+	},
+	blaziken: {
+		inherit: true,
+		tier: 'OU'
+	},
+	thundurus: {
+		inherit: true,
+		tier: 'OU'
+	}
+};
