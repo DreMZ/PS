db2a926e4a2bad778d9dcca0c1d0384b4728ac0b
Nollan101 <kgb@kgbudge.com>
2013-04-06 16:33:57 -0600

    Update moves.js

--- data/moves.js
+++ data/moves.js
@@ -13652,14 +13652,6 @@ exports.BattleMovedex = {
 		secondary: {
 			chance: 100,
 			volatileStatus: 'confusion'
-			boosts: {
-				atk: -6,
-				def: -6,
-				spa: -6,
-				spd: -6,
-				spe: -6,
-				accuracy: -6,
-				evasion: -6
 			self: {
 				boosts: {
 					atk: 6,
