7258d0f37f377e808959a083c6b4f7d4c7bc5b79
Nollan101 <kgb@kgbudge.com>
2013-04-06 14:54:05 -0600

    Create scripts.js

---
+++ mods/testmod/scripts.js
@@ -0,0 +1,134 @@
+exports.BattleScripts = {
+    init: function() {
+        for (var i in this.data.FormatsData)
+            this.data.FormatsData[i].dreamWorldRelease = true;
+            },
+                
+                this.data.Pokedex.clefable.types = ["Normal","Psychic"];
+                this.data.Pokedex.clefable.abilities['0'] = 'Friend Guard';
+                
+                this.data.Pokedex.arcanine.abilities['1'] = 'Adrenaline';
+                
+                this.data.Pokedex.electrode.abilities['1'] = 'Volt Absorb';
+                
+                this.data.Pokedex.exeggutor.abilities['1'] = 'Trickster';
+                
+                this.data.Pokedex.weezing.types = ["Poison","Electric"];
+                
+                this.data.Pokedex.chansey.abilities['1'] = 'Friend Guard';
+                
+                this.data.Pokedex.jynx.abilities['0'] = 'Caution';
+                
+                this.data.Pokedex.dragonair.types = ["Dragon","Water"];
+                
+                this.data.Pokedex.mewtwo.abilities['0'] = 'Dauntless';
+                
+                this.data.Pokedex.piloswine.abilities['0'] = 'Arctic Rush';
+                
+                this.data.Pokedex.skarmory.abilities['0'] = 'Ancient Wind';
+                
+                this.data.Pokedex.mantine.abilities['DW'] = 'Storm Drain';
+
+                this.data.Pokedex.kingdra.abilities['DW'] = 'Water Veil';
+                
+                this.data.Pokedex.blissey.abilities['1'] = 'Friend Guard';
+                
+                this.data.Pokedex.blaziken.abilities['DW'] = 'Adrenaline';
+                
+                this.data.Pokedex.gardevoir.abilities['0'] = 'Caution';
+                this.data.Pokedex.gardevoir.abilities['DW'] = 'Justified';
+                
+                this.data.Pokedex.cacturne.abilities['0'] = 'Sand Rush';
+ 
+                this.data.Pokedex.wailord.abilities['0'] = 'Hydration';
+                
+                this.data.Pokedex.claydol.abilities['0'] = 'Magic Guard';
+                this.data.Pokedex.claydol.abilities['DW'] = 'Caution';
+                
+                this.data.Pokedex.milotic.abilities['0'] = 'Multiscale';
+                
+                this.data.Pokedex.metagross.abilities['1'] = 'Light Metal';
+                this.data.Pokedex.metagross.abilities['DW'] = 'Gravotonize';
+                
+                this.data.Pokedex.regirock.abilities['DW'] = 'Sand Veil';
+               
+                this.data.Pokedex.registeel.abilities['DW'] = 'Heavy Metal';
+                
+                this.data.Pokedex.luxray.types = ["Electric","Dark"];
+                
+                this.data.Pokedex.bastiodon.types = ["Steel"];
+
+                this.data.Pokedex.drifblim.abilities['0'] = 'Tempest';
+
+                this.data.Pokedex.spiritomb.abilities['DW'] = 'Trickster';
+                
+                this.data.Pokedex.garchomp.abilities['DW'] = 'Mach Scale';
+                
+                this.data.Pokedex.hippowdon.abilities['DW'] = 'Sand Veil';
+
+                this.data.Pokedex.lumineon.types = ["Water","Flying"];
+
+                this.data.Pokedex.weavile.abilities['0'] = 'Technician';
+                
+                this.data.Pokedex.electivire.types = ["Electric","Fighting"];
+                
+                this.data.Pokedex.yanmega.types = ["Bug","Dragon"];
+                this.data.Pokedex.yanmega.abilities['1'] = 'Compoundeyes';
+                
+                this.data.Pokedex.glaceon.abilities['0'] = 'Arctic Rush';
+                
+                this.data.Pokedex.mamoswine.abilities['0'] = 'Arctic Rush';
+                
+                this.data.Pokedex.gallade.abilities['0'] = 'Caution';
+                this.data.Pokedex.gallade.abilities['1'] = 'Trace';
+                
+                this.data.Pokedex.dusknoir.types = ["Ghost","Fighting"];
+                this.data.Pokedex.dusknoir.abilities['0'] = 'Iron Fist';
+                
+                this.data.Pokedex.serperior.types = ["Grass","Dragon"];
+                
+                this.data.Pokedex.emboar.types = ["Fire","Dark"];
+                
+                this.data.Pokedex.musharna.types = ["Psychic","Ghost"];
+                this.data.Pokedex.musharna.abilities['1'] = 'Caution';
+                
+                this.data.Pokedex.darmanitan.types = ["Fire","Fighting"];
+                this.data.Pokedex.darmanitan.abilities['1'] = 'Reckless';
+                
+                this.data.Pokedex.sigilyph.abilities['0'] = 'Ancient Wind';
+                
+                this.data.Pokedex.cofagrigus.types = ["Ghost","Steel"];
+                
+                this.data.Pokedex.gothitelle.types = ["Psychic","Dark"];
+                
+                this.data.Pokedex.reuniclus.abilities['0'] = 'Gravotonize';
+                
+                this.data.Pokedex.zoroark.types = ["Dark","Ghost"];
+                this.data.Pokedex.zoroark.abilities['DW'] = 'Dauntless';
+                
+                this.data.Pokedex.swanna.abilities['0'] = 'Water Veil';
+                
+                this.data.Pokedex.vanilluxe.abilities['1'] = 'Snow Warning';
+                
+                this.data.Pokedex.beheeyem.types = ["Psychic","Electric"];
+                this.data.Pokedex.beheeyem.abilities['0'] = 'Levitate';
+                this.data.Pokedex.beheeyem.abilities['1'] = 'Dauntless';
+                
+                this.data.Pokedex.beartic.types = ["Ice","Fighting"];
+                this.data.Pokedex.beartic.abilities['0'] = 'Arctic Rush';
+                
+                this.data.Pokedex.cryogonal.types = ["Ice","Steel"];
+                
+                this.data.Pokedex.druddigon.types = ["Dragon","Rock"];
+                this.data.Pokedex.druddigon.abilities['0'] = 'Mach Scale';
+                this.data.Pokedex.druddigon.abilities['1'] = 'Solidify';
+                this.data.Pokedex.druddigon.abilities['DW'] = 'Marvel Scale';
+                
+                this.data.Pokedex.tornadustherian.abilities['0'] = 'Tempest';
+                
+                this.data.Pokedex.kyurem.abilities['0'] = 'Arctic Rush';
+                
+                this.data.Pokedex.genesect.abilities['0'] = 'Compoundeyes';
+        }
+};
+                
