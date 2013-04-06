d97d5e506f898ef7012669f345c5d8f196a3923c
Oiawesome <Oiawesome99@yahoo.com>
2013-04-06 12:49:27 -0700

    Create format-data.js

---
+++ mods/uuv2/format-data.js
@@ -0,0 +1,30 @@
+exports.BattleFormatsData = {
+  luxray: {
+    inherit: true,
+		tier: 'RU'
+	},
+	jynx: {
+		inherit: true,
+		tier: 'NU'
+	},
+	gothitelle: {
+		inherit: true,
+		tier: 'OU'
+	},
+	archeops: {
+		inherit: true,
+		tier: 'OU'
+	},
+	Wweavile: {
+		inherit: true,
+		tier: 'OU'
+	},
+	politoed: {
+		inherit: true,
+		tier: 'UU'
+	},
+	ninetales: {
+		inherit: true,
+		tier: 'UU'
+	}
+};
