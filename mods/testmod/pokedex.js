839d81920dc5ccad0ccb439479908d03a5b1b5f0
PokermansCoach <EngineerBLT@hotmail.com>
2013-04-06 21:49:47 +0200

    Create pokedex.js

---
+++ mods/testmod/pokedex.js
@@ -0,0 +1,3 @@
+exports.BattlePokedex = {
+dwebble:{num:557,species:"Dwebble",types:["Bug","Rock"],baseStats:{hp:255,atk:255,def:255,spa:255,spd:255,spe:255},abilities:{0:"Pure Power",1:"Magic Guard",DW:"Illusion"},heightm:0.3,weightkg:14.5,color:"Red",evos:["crustle"],eggGroups:["Bug","Mineral"]}
+};
