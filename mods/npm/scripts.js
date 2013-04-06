949903401a276e5ded6c1d08c618445e01d49200
Nollan101 <kgb@kgbudge.com>
2013-04-06 12:39:37 -0600

    Update scripts.js

--- mods/npm/scripts.js
+++ mods/npm/scripts.js
@@ -2,8 +2,24 @@ exports.BattleScripts = {
     init: function() {
         for (var i in this.data.FormatsData)
             this.data.FormatsData[i].dreamWorldRelease = true;
-    }
-
+         for (var i in this.data.Learnsets) {
+            delete this.data.Learnsets[i].fissure;
+},   
+         for (var i in this.data.Learnsets) {
+            delete this.data.Learnsets[i].sharpen;
+}, 
+         for (var i in this.data.Learnsets) {
+            delete this.data.Learnsets[i].lockon;
+}, 
+         for (var i in this.data.Learnsets) {
+            delete this.data.Learnsets[i].meditate;
+}, 
+         for (var i in this.data.Learnsets) {
+            delete this.data.Learnsets[i].flameburst;
+}, 
+         for (var i in this.data.Learnsets) {
+            delete this.data.Learnsets[i].howl;
+}  
                 this.data.Pokedex.charizard.types = ["Fire","Dragon"];
                 this.data.Pokedex.charizard.abilities['DW'] = 'Levitate';
                 this.data.Learnsets.charizard.learnset.dracometeor = ['5T'];
