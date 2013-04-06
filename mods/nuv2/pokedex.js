3a4c3b95b7b1faaa0c7abfd3fc9e396cd2ec0858
Oiawesome <Oiawesome99@yahoo.com>
2013-04-05 19:21:58 -0700

    Update pokedex.js

--- mods/nuv2/pokedex.js
+++ mods/nuv2/pokedex.js
@@ -1,125 +1,23 @@
 exports.BattlePokedex = {
-serperior: {
-                inherit:true,
-                abilities:{0:"Solar Power",1:"Synchronise",DW:"Leaf Guard"}
-                baseStats:{hp:98,atk:85,def:65,spa:95,spd:105,spe:103}
-        },
-articuno: {
-                inherit:true,
-                abilities:{0:"Snow Warning",DW:"Intimidate"}
-                baseStats:{hp:90,atk:35,def:88,spa:100,spd:130,spe:95}
-        },
-vulpix: {
-                inherit:true,
-                abilities:{0:"Drought",DW:"Magic Bounce"}
-                types:["Fire","Psychic"],
-                baseStats:{hp:80,atk:15,def:70,spa:90,spd:95,spe:50}
-        },
-carracosta: {
-                inherit:true,
-                abilities:{0:"Drizzle",DW:"Rock Head"}
-                baseStats:{hp:100,atk:105,def:130,spa:55,spd:65,spe:45}
-        },
-torterra: {
-                inherit:true,
-                abilities:{0:"Sand Stream",DW:"Sand Rush"}
-                types:["Grass","Ground"],
-                baseStats:{hp:90,atk:105,def:135,spa:35,spd:75,spe:56}
-        },
-girafarig: {
-                inherit:true,
-                abilities:{0:"Cursed Body",1:"Illusion",DW:"Rattled"}
-                types:["Normal","Ghost"],
-                baseStats:{hp:90,atk:90,def:90,spa:90,spd:90,spe:99}
-        },
-masquerain: {
-                inherit:true,
-                abilities:{0:"Telepathy",1:"Intimidate",DW:"Natural Cure"}
-                types:["Water","Bug"],
-                baseStats:{hp:80,atk:55,def:75,spa:103,spd:105,spe:88}
-        },
-simipour: {
-                inherit:true,
-                abilities:{0:"Rain Dish",1:"Cloud Nine",DW:"Water Absorb"}
-                types:["Water","Psychic"],
-                baseStats:{hp:100,atk:65,def:99,spa:88,spd:99,spe:69}
-        },
-simisage: {
-                inherit:true,
-                abilities:{0:"Natural Cure",1:"Leaf Guard",DW:"Sap Sipper"}
-                types:["Grass","Fighting"],
-                baseStats:{hp:70,atk:105,def:70,spa:60,spd:68,spe:100}
-        },
-simisear: {
-                inherit:true,
-                abilities:{0:"Flame Body",1:"Solar Power",DW:"Flash Fire"}
-                types:["Fire","Dark"],
-                baseStats:{hp:70,atk:60,def:70,spa:105,spd:68,spe:100}
-        },
-whirlipede: {
-                inherit:true,
-                abilities:{0:"Poison Point",1:"Poison Touch",DW:"Swarm"}
-                baseStats:{hp:70,atk:85,def:105,spa:25,spd:50,spe:67}
-        },
-linoone: {
-                inherit:true,
-                abilities:{0:"Pickup",1:"Gluttony",DW:"Sap Sipper"}
-                baseStats:{hp:70,atk:95,def:60,spa:25,spd:65,spe:100}
-        },
-butterfree: {
-                inherit:true,
-                abilities:{0:"Tinted Lens",1:"Compoundeyes",DW:"Shed Skin"}
-                baseStats:{hp:80,atk:65,def:65,spa:125,spd:70,spe:80}
-        },
-golurk: {
-                inherit:true,
-                abilities:{0:"Iron Fist",1:"Scrappy",DW:"Intimidate"}
-                types:["Ghost","Fighting"],
-                baseStats:{hp:100,atk:140,def:70,spa:55,spd:80,spe:65}
-        },
-frillish: {
-                inherit:true,
-                abilities:{0:"Magic Guard",1:"Dry Skin",DW:"Levitate"}
-                baseStats:{hp:85,atk:25,def:60,spa:90,spd:110,spe:65}
-        },
-tangela: {
-                inherit:true,
-                abilities:{0:"Regenerator",1:"Air Lock",DW:"Chlorophyll"}
-                baseStats:{hp:90,atk:15,def:131,spa:95,spd:45,spe:60}
-        },
-solorock: {
-                inherit:true,
-                abilities:{0:"Regenerator",1:"Leaf Guard",DW:"Magic Bounce"}
-                baseStats:{hp:80,atk:105,def:105,spa:60,spd:75,spe:85}
-        },
-jumpluff: {
-                inherit:true,
-                abilities:{0:"Sap Sipper",1:"Unburden",DW:"Leaf Guard"}
-                baseStats:{hp:70,atk:80,def:55,spa:80,spd:65,spe:105}
-        },
-dodrio: {
-                inherit:true,
-                abilities:{0:"Reckless",1:"Quick Feet",DW:"Gluttony"}
-                baseStats:{hp:65,atk:110,def:60,spa:35,spd:65,spe:105}
-        },
-electrode: {
-                inherit:true,
-                abilities:{0:"Lightningrod",1:"Volt Absorb",DW:"Telepathy"}
-                baseStats:{hp:65,atk:65,def:60,spa:105,spd:65,spe:150}
-        },
-primeape: {
-                inherit:true,
-                abilities:{0:"Intimidate",1:"Vital Spirit",DW:"Iron Fist"}
-                baseStats:{hp:70,atk:105,def:65,spa:80,spd:75,spe:98}
-        },
-scolipede: {
-                inherit:true,
-                abilities:{0:"Swarm",1:"Poison Touch",DW:"Insomnia"}
-                baseStats:{hp:70,atk:105,def:80,spa:20,spd:70,spe:115}
-        },
-munchlax: {
-                inherit:true,
-                abilities:{0:"Regenerator",1:"Thick Fat",DW:"Gluttony"}
-                baseStats:{hp:120,atk:80,def:60,spa:20,spd:105,spe:40}
-        }
+serperior:{num:497,species:"Serperior",types:["Grass","Psychic"],genderRatio:{M:0.875,F:0.125},baseStats:{hp:98,atk:85,def:70,spa:95,spd:105,spe:103},abilities:{0:"Solar Power",1:"Synchronize",DW:"Leaf Guard"},heightm:3.3,weightkg:63,color:"Green",prevo:"servine",evoLevel:36,eggGroups:["Ground","Plant"]},
+articuno:{num:144,species:"Articuno",types:["Ice","Flying"],gender:"N",baseStats:{hp:90,atk:35,def:88,spa:100,spd:130,spe:95},abilities:{0:"Snow Warning",DW:"Intimidate"},heightm:1.7,weightkg:55.4,color:"Blue",eggGroups:["No Eggs"]},
+vulpix:{num:37,species:"Vulpix",types:["Fire","Psychic"],genderRatio:{M:0.25,F:0.75},baseStats:{hp:80,atk:15,def:70,spa:90,spd:95,spe:50},abilities:{0:"Drought",DW:"Magic Bounce"},heightm:0.6,weightkg:9.9,color:"Brown",evos:["ninetales"],eggGroups:["Ground"]},
+carracosta:{num:565,species:"Carracosta",types:["Water","Rock"],genderRatio:{M:0.875,F:0.125},baseStats:{hp:100,atk:105,def:130,spa:55,spd:65,spe:45},abilities:{0:"Drizzle",DW:"Rock Head"},heightm:1.2,weightkg:81,color:"Blue",prevo:"tirtouga",evoLevel:37,eggGroups:["Water 1","Water 3"]},
+torterra:{num:389,species:"Torterra",types:["Grass","Ground"],genderRatio:{M:0.875,F:0.125},baseStats:{hp:90,atk:105,def:135,spa:35,spd:75,spe:56},abilities:{0:"Sand Stream",DW:"Sand Rush"},heightm:2.2,weightkg:310,color:"Green",prevo:"grotle",evoLevel:32,eggGroups:["Monster","Plant"]},
+girafarig:{num:203,species:"Girafarig",types:["Normal","Ghost"],baseStats:{hp:90,atk:90,def:90,spa:90,spd:90,spe:99},abilities:{0:"Cursed Body",1:"Illusion",DW:"Rattled"},heightm:1.5,weightkg:41.5,color:"Yellow",eggGroups:["Ground"]},
+masquerain:{num:284,species:"Masquerain",types:["Bug","Flying"],baseStats:{hp:75,atk:55,def:65,spa:103,spd:105,spe:87},abilities:{0:"Telepathy",1:"Intmidate",DW:"Natural Cure"},heightm:0.8,weightkg:3.6,color:"Blue",prevo:"surskit",evoLevel:22,eggGroups:["Water 1","Bug"]},
+simipour:{num:516,species:"Simipour",types:["Water"],genderRatio:{M:0.875,F:0.125},baseStats:{hp:100,atk:65,def:99,spa:88,spd:69,spe:101},abilities:{0:"Gluttony",DW:"Torrent"},heightm:1,weightkg:29,color:"Blue",prevo:"panpour",evoLevel:1,eggGroups:["Ground"]},
+simisage:{num:512,species:"Simisage",types:["Grass","Fighting"],genderRatio:{M:0.875,F:0.125},baseStats:{hp:70,atk:105,def:70,spa:60,spd:68,spe:100},abilities:{0:"Natural Cure",1:"Leaf Guard",DW:"Sap Sipper"},heightm:1.1,weightkg:30.5,color:"Green",prevo:"pansage",evoLevel:1,eggGroups:["Ground"]},
+simisear:{num:514,species:"Simisear",types:["Fire"],genderRatio:{M:0.875,F:0.125},baseStats:{hp:70,atk:60,def:70,spa:105,spd:68,spe:100},abilities:{0:"Gluttony",DW:"Blaze"},heightm:1,weightkg:28,color:"Red",prevo:"pansear",evoLevel:1,eggGroups:["Ground"]},
+whirlipede:{num:544,species:"Whirlipede",types:["Bug","Poison"],baseStats:{hp:70,atk:85,def:125,spa:65,spd:70,spe:85},abilities:{0:"Poison Point",1:"Poison Touch",DW:"Scrappy"},heightm:1.2,weightkg:58.5,color:"Gray",prevo:"venipede",evos:["scolipede"],evoLevel:22,eggGroups:["Bug"]},
+linoone:{num:264,species:"Linoone",types:["Normal"],baseStats:{hp:70,atk:95,def:60,spa:20,spd:65,spe:100},abilities:{0:"Pickup",1:"Gluttony",DW:"Sap Sipper"},heightm:0.5,weightkg:32.5,color:"White",prevo:"zigzagoon",evoLevel:20,eggGroups:["Ground"]},
+butterfree:{num:12,species:"Butterfree",types:["Bug","Flying"],baseStats:{hp:80,atk:65,def:65,spa:125,spd:70,spe:80},abilities:{0:"Compoundeyes",1:"Tinted Lens",DW:"Shed Skin"},heightm:1.1,weightkg:32,color:"White",prevo:"metapod",evoLevel:10,eggGroups:["Bug"]},
+golurk:{num:623,species:"Golurk",types:["Ghost","Fighting"],gender:"N",baseStats:{hp:100,atk:140,def:80,spa:55,spd:80,spe:70},abilities:{0:"Iron Fist",1:"Scrappy",DW:"Intimidate"},heightm:2.8,weightkg:330,color:"Green",prevo:"golett",evoLevel:43,eggGroups:["Mineral"]},
+frillish:{num:592,species:"Frillish",types:["Water","Ghost"],baseStats:{hp:85,atk:25,def:70,spa:90,spd:110,spe:65},abilities:{0:"Dry Skin",1:"Magic Guard",DW:"Levitate"},heightm:1.2,weightkg:33,color:"White",evos:["jellicent"],eggGroups:["Indeterminate"]},
+torkoal:{num:324,species:"Torkoal",types:["Fire","Rock"],baseStats:{hp:105,atk:85,def:145,spa:95,spd:55,spe:62},abilities:{0:"Solid Rock",1:"Flash Fire",DW:"Flame Body"},heightm:0.5,weightkg:80.4,color:"Brown",eggGroups:["Ground"]},
+tangela:{num:114,species:"Tangela",types:["Grass"],baseStats:{hp:90,atk:15,def:131,spa:95,spd:45,spe:60},abilities:{0:"Chlorophyll",1:"Air Lock",DW:"Regenerator"},heightm:1,weightkg:35,color:"Blue",evos:["tangrowth"],eggGroups:["Plant"]},
+jumpluff:{num:189,species:"Jumpluff",types:["Grass","Flying"],baseStats:{hp:70,atk:100,def:55,spa:80,spd:65,spe:105},abilities:{0:"Chlorophyll",1:"Leaf Guard",DW:"Unburden"},heightm:0.8,weightkg:3,color:"Blue",prevo:"skiploom",evoLevel:27,eggGroups:["Fairy","Plant"]},
+solrock:{num:338,species:"Solrock",types:["Rock","Psychic"],gender:"N",baseStats:{hp:80,atk:105,def:105,spa:60,spd:75,spe:85},abilities:{0:"Regenerator",1:"Leaf Guard",DW:"Magic Bounce"},heightm:1.2,weightkg:154,color:"Red",eggGroups:["Mineral"]},
+dodrio:{num:85,species:"Dodrio",types:["Normal","Flying"],baseStats:{hp:65,atk:110,def:60,spa:35,spd:65,spe:105},abilities:{0:"Reckless",1:"Quick Feet",DW:"Gluttony"},heightm:1.8,weightkg:85.2,color:"Brown",prevo:"doduo",evoLevel:31,eggGroups:["Flying"]},
+electrode:{num:101,species:"Electrode",types:["Electric"],gender:"N",baseStats:{hp:65,atk:65,def:60,spa:105,spd:65,spe:150},abilities:{0:"Lightningrod",1:"Volt Absorb",DW:"Telepathy"},heightm:1.2,weightkg:66.6,color:"Red",prevo:"voltorb",evoLevel:30,eggGroups:["Mineral"]},
 };
