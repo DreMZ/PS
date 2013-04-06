70de6016b31cdca51d3b9f29fb946d0ad8965aac
Nollan101 <kgb@kgbudge.com>
2013-04-06 14:51:26 -0600

    Create formats-data.js

---
+++ mods/testmod/formats-data.js
@@ -0,0 +1,46 @@
+exports.BattleFormatsData = {
+        kyurem: {
+                inherit: true,
+                tier: 'Uber'
+        },
+        kyuremblack: {
+                inherit: true,
+                tier: 'Uber'
+        },
+        regigigas: {
+                inherit: true,
+                tier: 'Uber'
+        },
+        meloettapirouette: {
+                inherit: true,
+                tier: 'Uber'
+        },
+        latias: {
+                inherit: true,
+                tier: 'Uber'
+        },
+        latios: {
+                inherit: true,
+                tier: 'Uber'
+        },
+        genesect: {
+                inherit: true,
+                tier: 'OU'
+        },
+        blaziken: {
+                inherit: true,
+                tier: 'OU'
+        },
+        excadrill: {
+                inherit: true,
+                tier: 'OU'
+        },
+        tornadustherian: {
+                inherit: true,
+                tier: 'OU'
+        },
+        thundurus: {
+                inherit: true,
+                tier: 'OU'
+        }
+};
